12c33ca32b123857d287a7a4b7b59077999a6ea0
Batya Zamansky <batya.zamansky@gmail.com>
2013-03-22 13:13:26 -0400

    did basic app stuff

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ app.secret_key = "blah"
 def home():
     return render_template("home.html")
 
+@app.route("/profile", methods = ["GET"])
+def profile():
+    return render_template("profile.html")
+
+@app.route("/generate", methods = ["GET","POST"])
+def generate():
+    return render_template("makepoem.html")
+
+if __name__ == '__main__':
+    app.run(debug = True, host="0.0.0.0", port = 5000)
