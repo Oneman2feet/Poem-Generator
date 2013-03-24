7c4d5f250138cb993913314c9afaa964ab2cccf1
Batya Zamansky <batya@turtle.(none)>
2013-03-24 14:11:44 -0400

    I HAVE SOMETHING THAT GIVES THE NUM OF SYLLABLES PER WORD YAY

--- nltktest.py
+++ nltktest.py
@@ -1,5 +1,6 @@
-import nltk
-
+import nltk, re, nltk.data,curses
+from curses.ascii import isdigit
+from nltk.corpus import cmudict
 
 """
 ADJ	adjective	
@@ -25,16 +26,37 @@ WH	wh determiner
 
 
 
-sentence = "Everyday each one of you will be E-mailed a name. That is the name of the person whom you are to seek out and try to mark with a marker or highlighter. Once you have successfully marked that person, he/she should tell you the name of his/her target, which now becomes your new target. The person whom you just marked is eliminated from the game. Remember that while you are on the lookout for your target, you yourself are also being searched for. Your objective is to remain unmarked and become the last man standing in which case you win!  If you see yourself or another person about to be marked, you may say the word Assassin, and you or that person will be immune for the duration of that class period, ONLY from the person who tried to mark you or another person at the time you said Assassin."
+sentence = "Everyday each one of you will be Emailed a name. That is the name of the person whom you are to seek out and try to mark with a marker or highlighter. Once you have successfully marked that person, he/she should tell you the name of his/her target, which now becomes your new target. The person whom you just marked is eliminated from the game. Remember that while you are on the lookout for your target, you yourself are also being searched for. Your objective is to remain unmarked and become the last man standing in which case you win!  If you see yourself or another person about to be marked, you may say the word Assassin, and you or that person will be immune for the duration of that class period, ONLY from the person who tried to mark you or another person at the time you said Assassin."
 
 tokens = nltk.word_tokenize(sentence)
 
 tagged = nltk.pos_tag(tokens)
 
-print tagged
+#print tagged
 
 verbs = [x for x in tagged if x[1] == 'VB']
 #print verbs
 
 PRP = [x for x in tagged if x[1] == 'PRP']
-print PRP
+#print PRP
+
+
+d = cmudict.dict()
+def nysl(word):
+    try:
+        return [len(list(y for y in x if isdigit(y[-1]))) for x in d[word.lower()]]
+    except:
+        return "word is not in cmudict"
+
+regexp = "[A-Za-z]+"
+exp = re.compile(regexp)
+print tokens
+
+for a in tokens:
+    a = a.lower()
+    if a[-1] == ".":
+        a = a[:-1]
+    if exp.match(a):
+        print a +" " + str(nysl(a)[0])
+    else:
+        print a
