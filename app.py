499f75aeed8e52f87e4f3dc365e29f48922da868
Amanda <vamandaboss1@gmail.com>
2013-04-06 16:05:52 -0400

    edits

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template, request
-import init
+from flask import Flask, render_template, request, redirect, session
+
 
 app = Flask(__name__)
 app.secret_key = "blah"
 
-@app.route("/", methods = ["GET"])
+global current_user
+
+@app.route("/", methods = ["GET", "POST"])
 def home():
+    #this is the code for using session when db is written
+    #utils.connect()
+    #if request.method == "GET":
+    #   poems = [str(init.makeHaiku()) for x in range(0,10)]
+    #   return render_template("home.html", poems=poems)
+    #else:
+    #   global current_user
+    #   button = str(request.form["button"])
+    #   if button == "Login":
+    #       username = request.form.get("username")
+    #       password = request.form.get("password")
+    #       if users.signup(username, password):
+    #           poems = utils.get_poems(current_user)
+    #           return redirect("/"+current_user) I don't know if you want to use this
+    #   else:
+    #       return "Username is taken"
+    
+    
     poems = [str(init.makeHaiku()) for x in range(0,10)]
     return render_template("home.html", poems=poems)
 
