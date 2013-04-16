ba47c000843a9850ae57a6a214edca76cf46fd8f
Batya Zamansky <batya.zamansky@gmail.com>
2013-04-16 13:17:33 -0400

    fixed login and register but need to fix all buttons

--- app.py
+++ app.py
@@ -4,35 +4,53 @@ import init, mongo
 app = Flask(__name__)
 app.secret_key = "blah"
 
-global current_user
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
-    #this is the code for using session when db is written
-    mongo.conn()
-
-    #if request.method == "GET":
-    global current_user
-    button = str(request.form["button"])
-    if button == "Login":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        if mongo.exists(username, password):
-            poems = mongo.get_poems(current_user)
-            return redirect("/"+current_user) #might change
-        else:
-            return "Username is taken"
-       
+    #change this to 10 of most recent poems
     poems = [str(init.makeHaiku()) for x in range(0,10)]
+
+    if request.method == "POST":
+        button = request.form['button']
+        if button == 'Login':
+            username = request.form.get("username")
+            password = request.form.get("password")
+            if mongo.exists(username, password):
+                session['user'] = username
+                return redirect("profile.html")
+            else:
+                return "Username is taken"
+        elif button == "Register":
+            return redirect("register.html")
     return render_template("home.html", poems=poems)
 
 @app.route("/profile", methods = ["GET"])
 def profile():
-    return render_template("profile.html")
+    if 'user' in session:
+        return render_template("profile.html")
+    else:
+        print "User Not Logged In"
+        return redirect("home.html")
 
 @app.route("/generate", methods = ["GET","POST"])
 def generate():
     return render_template("makepoem.html", user = user)
 
+@app.route("/register",methods = ["GET","POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        mongo.addUser(username,password)
+        session['user'] = username
+        return redirect("profile")
+    
+    return render_template("register.html")
+
 if __name__ == '__main__':
     app.run(debug = True, host="0.0.0.0", port = 5000)
+
+
+
+####Fix buttons
+####
