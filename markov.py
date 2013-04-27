c60fdcd453656c5efa91643c774d2dd9c0bc5ed6
Batya Zamansky <batya@turtle.(none)>
2013-04-27 13:27:37 -0400

    cool we can do markov chains

--- markov.py
+++ markov.py
@@ -3,21 +3,22 @@ import random
 #with keys being the seed words
 #and data being the words that have come after that word
 
-shakespeare = {}
 
-def makecorpus():
-    sonnets = open("sonnets.txt").readlines()
-    sonnets = [ [word.lower().replace('(','').replace(')','')
+def makecorpus(filename):
+    name  = {}
+    poems = open(filename).readlines()
+    poems = [ [word.lower().replace('(','').replace(')','')
                  for word in line.replace('\n',' \n').split(' ')
                  if word!='' and word!='\n']
-                 for line in sonnets if line!='\n' ]
+                 for line in poems if line!='\n' ]
 
-    for line in sonnets:
+    for line in poems:
         for i in xrange(0,len(line)-1):
-            if line[i] in shakespeare.keys():
-                shakespeare[line[i]].append(line[i+1])
+            if line[i] in name.keys():
+                name[line[i]].append(line[i+1])
             else:
-                shakespeare[line[i]] = [line[i+1]]
+                name[line[i]] = [line[i+1]]
+    return name
 
 
 def makeline(startword,corpus):
@@ -45,6 +46,6 @@ def makepoem(n,corpus):
     return poem
 
 if __name__ == '__main__':
-    makecorpus()
+    shakespeare = makecorpus("sonnets.txt")
     #print shakespeare
     print makepoem(8,shakespeare)
