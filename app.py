b3740b888da1e2f9c51c997f9756a35f18025f00
Zachary <oneman2feet@gmail.com>
2013-05-03 23:47:51 +0000

    i made changes?

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def logout():
     return redirect("/")
 
 if __name__ == '__main__':
-    app.run(debug = True, host="0.0.0.0", port = 7999)
+    app.run(debug = False, host="0.0.0.0", port = 7999)
 
 
 
